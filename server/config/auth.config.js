93af9b8d694ff439af9f5941c3b5ed883395d1e9
Georgi Tsanev <cologneto@gmail.com>
2020-12-29 14:48:34 +0200

    Middlewares and auth.controller added

---
+++ server/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "cologneto-security-key"
+}
--- server/server.js
+++ server/server.js
@@ -40,3 +40,30 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
